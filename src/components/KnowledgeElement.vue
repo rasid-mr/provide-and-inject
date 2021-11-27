e0d5c32ee916731a428bde5093371b23d3e616c2
mohtasim <mohtasim.chemist@gmail.com>
2021-11-27 09:20:42 +0600

    first

---
+++ src/components/KnowledgeElement.vue
@@ -0,0 +1,17 @@
+<template>
+  <li>
+    <h3>{{ topicName }}</h3>
+    <p>{{ description }}</p>
+    <button @click="selectedTopic(id)">Learn More</button>
+  </li>
+</template>
+
+<script>
+export default {
+  inject: ['selectedTopic'],
+  props: ['id', 'topicName', 'description']
+  // emits: ['select-topic'],
+};
+</script>
+
+// <button @click="$emit('select-topic', id)"></button>
